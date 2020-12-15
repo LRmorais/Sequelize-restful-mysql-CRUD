ff8c6ec6619d5ab2a6ec3cb4a6ee1b8b735db029
Lucas Morais <lucas.morais23ti@gmail.com>
2020-12-15 11:57:36 -0300

    add metodo post

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,35 @@
+// importa tabelas e atributos criados em usuario.js
+const Usuario = require('../models/usuario.js');
+// biblioteca utilizada para tratamentos de erros
+const status = require('http-status');
+
+
+// Insert é um nome qualquer escolhido para definir um dos metodos get,post,delete ou update
+// req -> requisição -> será um json armazenado posteriormente em uma variavel, res-> resposta
+    exports.Insert = (req, res, next) => {
+
+        // requisição.corpoDaRequisição.atributo
+        const nome = req.body.nome;
+        const salario = req.body.salario;
+        const dataNascimento = req.body.dataNascimento;
+        const ativo = req.body.ativo;
+
+        // metodo de post do sequelize 
+        Usuario.create({
+            // primeiro nome é a propriedade do modelo(atributo no bd), segundo nome é a variavel com os dados armazenados
+            nome: nome,
+            salario: salario,
+            dataNascimento: dataNascimento,
+            ativo: ativo
+        })
+        // tratamento de erros
+        .then(usuario => {
+            if(usuario){
+                res.status(status.OK).send(usuario);
+            } else{
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+    }
+    
